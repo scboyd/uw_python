a99897fbf5813b7756d1b3135e403f31a36b1a44
Scott Boyd <scott.p.boyd@live.com>
2012-01-16 18:10:46 -0800

    add recho directory with Week2 Lab (part2) code

--- recho/recho_client.py
+++ recho/recho_client.py
@@ -20,15 +20,18 @@ if nargs > 1:
 if nargs > 2:
     port = int(sys.argv[2])
 
-s = socket.socket(socket.AF_INET, 
+while True:
+    s = socket.socket(socket.AF_INET, 
                   socket.SOCK_STREAM) 
-s.connect((host,port)) 
 
-input = raw_input("Enter text to echo: ")
-s.send(input)
 
-s.send('Hello, world') 
-data = s.recv(size) 
-s.close() 
-print 'from (%s,%s) %s' % (host, port, data)
+    s.connect((host,port)) 
+    input = raw_input("Enter text to echo: ")
+    if not input:
+        print "Quiting"
+        sys.exit(1)
+    s.send(input)
+    data = s.recv(size) 
+    s.close() 
+    print 'from (%s,%s) %s' % (host, port, data)
 
